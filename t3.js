94e6ab2ecd6be78448d85c8a84aa5ab814c63cce
arorayat <87564566+arorayat@users.noreply.github.com>
2021-09-17 19:55:39 +1000

    Add files via upload

---
+++ t3.js
@@ -0,0 +1,77 @@
+const text1 = 'hello';
+const text2 = 'world';
+const text3 = '     JavaScript    ';
+
+// concatenating two strings
+const result1 = text1.concat(' ', text2);
+console.log(result1); // "hello world"
+
+// converting the text to uppercase
+const result2 = text1.toUpperCase();
+console.log(result2); // HELLO
+
+// removing whitespace from the string
+const result3 = text3.trim();
+console.log(result3); // JavaScript
+
+// converting the string to an array
+const result4 = text1.split();
+console.log(result4); // ["hello"]
+
+// slicing the string
+const result5= text1.slice(1, 3);
+console.log(result5); // "el"
+
+
+
+Write a JavaScript program which prints the elements of the following array.
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>Print 2-D array</title>
+</head>
+<body>
+</body>
+</html>
+
+JavaScript Code:
+
+// a sample 2-D array 
+var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+ 
+for (var i in a) 
+{
+   console.log("row " + i);
+   for (var j in a[i]) 
+     {
+      console.log(" " + a[i][j]);
+     }
+}
+
+3.
+const student = {
+    firstName: 'Monica'
+}
+
+// getting property
+Object.defineProperty(student, "getName", {
+    get : function () {
+        return this.firstName;
+    }
+});
+
+// setting property
+Object.defineProperty(student, "changeName", {
+    set : function (value) {
+        this.firstName = value;
+    }
+});
+
+console.log(student.firstName); // Monica
+
+// changing the property value
+student.changeName = 'Sarah';
+
+console.log(student.firstName); // Sarah
